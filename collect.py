29308309eeadb7532b7feadc36ffe3832b183431
Graeham Rieman <graeham.rieman@aunalytics.com>
2017-07-12 15:49:33 -0400

    Collect ready for testing

--- collect.py
+++ collect.py
@@ -3,24 +3,26 @@ import pandas as pd
 import time
 
 def get_data():
-    # rpm = obd.commands.RPM
-    # fuel = obd.commands.FUEL_STATUS
-    # coolant = obd.commands.COOLANT_TEMP
-    # speed = obd.commands.SPEED
-    # intake = obd.commands.INTAKE_STATUS
-    # run_time = obd.commands.RUN_TIME
-    # throttle = obd.commands.THROTTLE_POS
-    # fuel_press = obd.commands.FUEL_PRESSURE
-    # intake_press = obd.commands.INTAKE_PRESSURE
-    # air_flow_rate = obd.commands.MAF
-    # ambiant_temp = obd.commands.AMBIANT_AIR_TEMP
-    # ethanol = obd.commands.ETHANOL_PERCENT
-    # oil_temp = obd.commands.OIL_TEMP
-    # fuel_consumption = obd.commands.FUEL_RATE
     rpm = connection.query(obd.commands.RPM)
-    out_row = pd.Series([rpm.time, rpm.value])
+    fuel = connection.query(obd.commands.FUEL_STATUS)
+    coolant = connection.query(obd.commands.COOLANT_TEMP)
+    speed = connection.query(obd.commands.SPEED)
+    intake = connection.query(obd.commands.INTAKE_STATUS)
+    run_time = connection.query(obd.commands.RUN_TIME)
+    throttle = connection.query(obd.commands.THROTTLE_POS)
+    fuel_press = connection.query(obd.commands.FUEL_PRESSURE)
+    intake_press = connection.query(obd.commands.INTAKE_PRESSURE)
+    air_flow_rate = connection.query(obd.commands.MAF)
+    ambiant_temp = connection.query(obd.commands.AMBIANT_AIR_TEMP)
+    ethanol = connection.query(obd.commands.ETHANOL_PERCENT)
+    oil_temp = connection.query(obd.commands.OIL_TEMP)
+    fuel_consumption = connection.query(obd.commands.FUEL_RATE)
+    
+    out_row = pd.Series([rpm.time, rpm.value, fuel.value, coolant.value,
+          speed.value, intake.value, run_time.value, throttle.value, fuel_press.value,
+          intake_press.value, air_flow_rate.value, ambiant_temp.value, ethanol.value,
+          oil_temp.value, fuel_consumption.value])
     return out_row
-
 
 
 
@@ -41,9 +43,10 @@ try:
       dataset = dataset.append(next_row, ignore_index=True)
       if next_row[1] < 1:
         car_on = False
-        # save as csv
+        filename = time.asctime(time.localtime(next_row[0])) + "_data.csv"
+        dataset.to_csv(filename, sep="|")
     else:
-      time.sleep(10.0 - ((time.time() - starttime) % 10.0))
+      time.sleep(20.0 - ((time.time() - starttime) % 20.0))
       if next_row[1] > 0:
         car_on = True
         dataset = pd.DataFrame().append(next_row, ignore_index=True)
